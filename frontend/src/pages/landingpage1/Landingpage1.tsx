5fcf0c1d71e407643239e86b559e5e0846324e42
Mohammed Owais <owaismohammed795@gmail.com>
2024-05-16 00:03:12 +0530

    Corrected the card scroll for mobile

--- frontend/src/pages/landingpage1/Landingpage1.tsx
+++ frontend/src/pages/landingpage1/Landingpage1.tsx
@@ -1,12 +1,20 @@
 import './Landingpage1.css'
 import AutoSlider from '../../components/AutoSlider'
 import Card from '../../components/Card'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import {ScrollMenu} from 'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css';
 
 
 const  Cards = [{
+  imgurl: '/src/assets/images/card-img.jpg',
+  title: 'TechVistara',
+  venue: 'Seminar Hall',
+  date: '2/19/2024',
+  participant1: '/src/assets/images/profile-img1.jpg',
+  participant2: '/src/assets/images/profile-img2.jpg',
+  participant3: '/src/assets/images/profile-img2.jpg'
+ },
+ {
   imgurl: '/src/assets/images/card-img.jpg',
   title: 'TechVistara',
   venue: 'Seminar Hall',
@@ -46,40 +54,6 @@ const  Cards = [{
 
 
 function Landingpage1() {
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
   return (
     <div className='lp1-main'>
       <div className="auto-div">
@@ -87,11 +61,11 @@ function Landingpage1() {
       </div>
 
       <div className='cards-main'>
-        <Slider {...settings}>
+        <ScrollMenu>
           {Cards.map((card) => {
             return <Card imgurl={card.imgurl} title={card.title} venue={card.venue} date={card.date} participants={[card.participant1, card.participant2, card.participant3]} />
           })}
-        </Slider>
+        </ScrollMenu>
       </div>
     </div>
   )
